d71b8555a6a0605e4cef0d6e12c8001abc2cd76c
kurrbanov <justnick1312@gmail.com>
2022-07-26 12:49:01 +0300

    feat: added the car represent in admin

--- parser/admin.py
+++ parser/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
+from django.utils.html import mark_safe
 
-# Register your models here.
+from parser.models import CarGeneration
+
+
+@admin.register(CarGeneration)
+class CarGenerationAdmin(ModelAdmin):
+    list_display = ('image_car', '__str__', 'average_price', 'total', 'auto_ru_link')
+
+    def image_car(self, obj: CarGeneration):
+        return mark_safe(f'<img src="{obj.photo_link}" width="300px" height="200px">')
